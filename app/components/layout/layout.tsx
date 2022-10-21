ce9b9e9a7b13c201c0b864f8e010120cd7542305
Hans Brems <hansbrems@gmail.com>
2022-10-21 18:23:38 +0200

    Add footer

--- app/components/layout/layout.tsx
+++ app/components/layout/layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from '@remix-run/react';
 import { Header, links as headerLinks } from '../header/header';
+import { Footer, links as footerLinks } from '../footer/footer';
 import styles from './layout.css';
 
 export const links = () => [
   ...headerLinks(),
+  ...footerLinks(),
   { rel: 'stylesheet', href: styles },
 ];
 
@@ -11,10 +13,10 @@ export function Layout() {
   return (
     <>
       <Header />
-
       <main className="container">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
