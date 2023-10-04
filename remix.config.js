199edaff882f122c35c87a09aba98d5f31c6256c
Hans Brems <hansbrems@gmail.com>
2023-10-04 12:37:01 +0200

    Update Remix

--- remix.config.js
+++ remix.config.js
@@ -7,6 +7,14 @@ module.exports = {
   publicPath: '/build/',
   serverBuildDirectory: 'netlify/functions/server/build',
   devServerPort: 8002,
+  future: {
+    v2_dev: true,
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
   mdx: async filename => {
     const [rehypeHighlight] = await Promise.all([
       import('rehype-highlight').then(mod => mod.default),
